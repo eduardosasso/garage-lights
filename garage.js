8a506585054f65a4a57a5d18edfac639a8e5f33f
blakbelt78 <blakbelt78@gmail.com>
2019-02-09 01:31:48 +0000

    improve code a bit

--- garage.js
+++ garage.js
@@ -1,17 +1,26 @@
 // /warning this may trigger multiple times for one press
 //...usually triggers twice based on testing for each press
-var dash_button = require('node-dash-button'yyygtygyrry
-var dash = dash_button("a0:02:dc:76:c8:71", null, 20000, 'udp'); //address from step above
-var toggle = "off"
+const dash_button = require('node-dash-button')
+const dash = dash_button("a0:02:dc:76:c8:71", null, 20000, 'udp') //address from step above
+
+const request = require('axios')
+
+const ifttt_turn_on = 'https://maker.ifttt.com/trigger/button_pressed_on/with/key/3doIG3PwHgFIE2oN8OUb59JLq2gLIN9uEzQYUc2DhE'
+const ifttt_turn_off = 'https://maker.ifttt.com/trigger/button_pressed/with/key/3doIG3PwHgFIE2oN8OUb59JLq2gLIN9uEzQYUc2DhE'
+
+const ON = true
+const OFF = false
+
+let light_switch = OFF
+
 dash.on("detected", function (){
-  console.log("omg found");
-  if (toggle == "off") {
-		axios.post('https://maker.ifttt.com/trigger/button_pressed_on/with/key/3doIG3PwHgFIE2oN8OUb59JLq2gLIN9uEzQYUc2DhE').then(function (response) {
-			toggle = "on"
+  if (light_switch == OFF) {
+    request.post(ifttt_turn_on).then(function (response) { 
+      light_switch = ON
 		});
 	} else {
-		axios.post('https://maker.ifttt.com/trigger/button_pressed/with/key/3doIG3PwHgFIE2oN8OUb59JLq2gLIN9uEzQYUc2DhE').then(function (response) {
-			toggle = "off"
+		request.post(ifttt_turn_off).then(function (response) {
+      light_switch = OFF
 		});
 	}
 });
